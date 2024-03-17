1f1499d6765b09b4085cf6e7642d21de60a9fc94
Strafer1337 <vova.ilin2000@yandex.ru>
2024-03-17 13:30:29 +0300

    task007 done+improved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,32 +6,32 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val changeButton: Button = findViewById(R.id.changeButton)
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        titleTextView = findViewById(R.id.titleTextView)
 
-//        if (savedInstanceState != null)
-//            titleTextView.text = savedInstanceState.getString("ttw")
+        savedInstanceState?.let {
+            titleTextView.text = it.getString("ttw")
+        }
 
         changeButton.setOnClickListener {
             titleTextView.text = getString(R.string.and_dev)
         }
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
-        titleTextView.text = savedInstanceState.getString("ttw")
-    }
-
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//
+//        val titleTextView: TextView = findViewById(R.id.titleTextView)
+//        titleTextView.text = savedInstanceState.getString("ttw")
+//    }
+//
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
         outState.putString("ttw", titleTextView.getText().toString())
     }
 }
