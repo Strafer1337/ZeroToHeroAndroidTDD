7dbd4e73168efe703e3295c1253bac1266f4470d
Strafer1337 <vova.ilin2000@yandex.ru>
2024-03-17 14:38:00 +0300

    task009 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,37 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
 import android.os.Bundle
-import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rootLayout: ViewGroup
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val hideButton: Button = findViewById(R.id.hideButton)
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        rootLayout = findViewById(R.id.rootLayout)
+        titleTextView = findViewById(R.id.titleTextView)
+        val removeButton: Button = findViewById(R.id.removeButton)
 
-//        if (savedInstanceState != null)
-//            titleTextView.text = savedInstanceState.getString("ttw")
-        hideButton.setOnClickListener {
-            titleTextView.visibility = View.INVISIBLE
+        removeButton.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+        }
+
+        savedInstanceState?.let {
+            val isRemoved = savedInstanceState.getBoolean("isRemoved")
+            if (isRemoved)
+                rootLayout.removeView(titleTextView)
         }
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
-        titleTextView.visibility = savedInstanceState.getInt("ttw_visibility")
+        // titleTextView.visibility = savedInstanceState.getInt("ttw_visibility")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
-        outState.putInt("ttw_visibility", titleTextView.visibility)
+        outState.putBoolean("isRemoved", findViewById<TextView>(R.id.titleTextView) == null)
     }
 }
