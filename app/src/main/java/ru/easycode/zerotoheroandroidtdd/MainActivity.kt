33206e69c3a3edcf82f534b78fd841db4a9f1a11
Strafer1337 <vova.ilin2000@yandex.ru>
2024-03-17 13:34:32 +0300

    task008 done+improved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,31 +7,24 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val hideButton: Button = findViewById(R.id.hideButton)
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        titleTextView = findViewById(R.id.titleTextView)
 
-//        if (savedInstanceState != null)
-//            titleTextView.text = savedInstanceState.getString("ttw")
+        savedInstanceState?.let {
+            titleTextView.visibility = savedInstanceState.getInt("ttw_visibility")
+        }
+
         hideButton.setOnClickListener {
-            titleTextView.visibility = View.INVISIBLE
+            titleTextView.visibility = View.INVISIBLE // or GONE
         }
     }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
-        titleTextView.visibility = savedInstanceState.getInt("ttw_visibility")
-    }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
-        val titleTextView: TextView = findViewById(R.id.titleTextView)
         outState.putInt("ttw_visibility", titleTextView.visibility)
     }
 }
