b60f7b00c239114511a23dc561684469497f3905
Strafer <vova.ilin2000@yandex.ru>
2024-03-19 16:59:56 +0300

    Task010 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -9,22 +9,25 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private lateinit var rootLayout: ViewGroup
     private lateinit var titleTextView: TextView
+    private lateinit var removeButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rootLayout = findViewById(R.id.rootLayout)
         titleTextView = findViewById(R.id.titleTextView)
-        val removeButton: Button = findViewById(R.id.removeButton)
+        removeButton = findViewById(R.id.removeButton)
 
         removeButton.setOnClickListener {
             rootLayout.removeView(titleTextView)
+            removeButton.isEnabled = false
         }
 
         savedInstanceState?.let {
-            val isRemoved = savedInstanceState.getBoolean("isRemoved")
-            if (isRemoved)
-                rootLayout.removeView(titleTextView)
+            val isRemovedTextView = savedInstanceState.getBoolean("isRemovedTTW")
+            val isRemovedButton = savedInstanceState.getBoolean("isRemovedBtn")
+            if (!isRemovedButton) removeButton.isEnabled = false
+            if (isRemovedTextView) rootLayout.removeView(titleTextView)
         }
     }
 
@@ -35,6 +38,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean("isRemoved", findViewById<TextView>(R.id.titleTextView) == null)
+        outState.putBoolean("isRemovedTTW", findViewById<TextView>(R.id.titleTextView) == null)
+        outState.putBoolean("isRemovedBtn", removeButton.isEnabled)
     }
 }
