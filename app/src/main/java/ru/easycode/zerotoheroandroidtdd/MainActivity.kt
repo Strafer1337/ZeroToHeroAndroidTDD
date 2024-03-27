b6c856d36f58c0bd7cf655ebed5e3c888879da6b
Strafer <vova.ilin2000@yandex.ru>
2024-03-27 07:08:58 +0300

    Task014 first thoughts

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val titleTextView: TextView = findViewById(R.id.titleTextView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val actionButton: Button = findViewById(R.id.actionButton)
+
+        actionButton.setOnClickListener {
+            actionButton.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+
+            SystemClock.sleep(2000);
+
+            titleTextView.visibility = View.VISIBLE
+            actionButton.isEnabled = true
+            progressBar.visibility = View.GONE
+        }
     }
 }
