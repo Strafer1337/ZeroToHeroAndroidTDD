f28911b3d8c3777c058b1fdee7926b40cfddf656
Strafer <vova.ilin2000@yandex.ru>
2024-03-27 09:21:12 +0300

    Task014 done (2 versions)

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,12 +1,13 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +22,19 @@ class MainActivity : AppCompatActivity() {
             actionButton.isEnabled = false
             progressBar.visibility = View.VISIBLE
 
-            SystemClock.sleep(2000);
-
-            titleTextView.visibility = View.VISIBLE
-            actionButton.isEnabled = true
-            progressBar.visibility = View.GONE
+            actionButton.postDelayed({
+                titleTextView.visibility = View.VISIBLE
+                actionButton.isEnabled = true
+                progressBar.visibility = View.GONE
+            }, 3000)
+/*
+* вторая версия
+ */
+//            Handler(Looper.getMainLooper()).postDelayed({
+//                titleTextView.visibility = View.VISIBLE
+//                actionButton.isEnabled = true
+//                progressBar.visibility = View.GONE
+//            }, 3000)
         }
     }
 }
